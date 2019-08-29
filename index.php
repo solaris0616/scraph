47b319f81c6fe3c4826241d46c35ee565270f209
solaris0616 <solaris0616@gmail.com>
2019-08-29 17:13:09 +0900

    Implement MaterializeCSS

--- index.php
+++ index.php
@@ -31,29 +31,86 @@ try {
 }
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="ja">
     <head>
-        <title>scraph</title>
+        <title>Scraph</title>
         <meta charset="utf-8">
+        <meta name="description" content="Store your favorite sites like a scrapbook.">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-        <h1>scraph</h1>
-        <form action="regist.php" method="post">
-            <p>URL:</p>
-            <input type="text" name="url">
-            <p>Comment:</p>
-            <textarea type="text" name="body"></textarea>
-            <input type="submit" value="Regist">
-        </form>
-        <h1>Entries</h1>
+        <!-- navigation bar -->
+        <nav class="teal darken-1">
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="#!" class="brand-logo">Scraph</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <!-- cards -->
+            <div class="row">
 <?php foreach($entries as $entry): ?>
-        <h4>
-            <a href="<?= htmlspecialchars($entry->getUrl()) ?>" target="_blank">
-                <?= htmlspecialchars($entry->getTitle()) ?>
-            </a>
-        </h4>
-        <p><?= htmlspecialchars($entry->getBody()) ?></p>
-        <a href="./delete.php?id=<?= $entry->getId() ?>">削除</a>
+                <div class="col s12">
+                    <div class="card">
+                        <div class="card-content">
+                            <span class="card-title">
+                                <a href="<?= htmlspecialchars($entry->getUrl()) ?>" target="_blank">
+                                    <?= htmlspecialchars($entry->getTitle()) ?>
+                                </a>
+                            </span>
+                            <p><?= htmlspecialchars($entry->getBody()) ?></p>
+                        </div>
+                        <div class="card-action">
+                            <a href="./delete.php?id=<?= $entry->getId() ?>">削除</a>
+                        </div>
+                    </div>
+                </div>
 <?php endforeach ?>
+            </div>
+
+            <!-- action button -->
+            <div class="fixed-action-btn">
+                <a class="btn-floating btn-large modal-trigger" href="#reg-modal">
+                   <i class="large material-icons">add</i>
+                </a>
+            </div>
+
+            <!-- registration modal -->
+            <div id="reg-modal" class="modal">
+                <div class="modal-content">
+                    <form id="reg-form" class="col s12" action="regist.php" method="post">
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">account_circle</i>
+                                <input id="url" name="url" type="text">
+                                <label for="url">URL</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">mode_edit</i>
+                                <textarea class="materialize-textarea" name="body" id="body"></textarea>
+                                <label for="body">Comment</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button form="reg-form" type="submit" class="btn waves-effect waves-light modal-close">
+                        Submit<i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script src="./script.js"></script>
     </body>
 </html>
